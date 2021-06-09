16968b0aa56abab82aef3795cdc6193e817ce08c
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-06-10 00:22:49 +0200

    Add movie and person

--- src/views/Movie.js
+++ src/views/Movie.js
@@ -1,10 +1,44 @@
 import React, {Component} from "react";
+import "../assets/css/Movie.css";
+import getMovies from "../api/endpoints/getMovies";
 
 export default class Movie extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            movies: []
+        };
+    }
+
+    async componentDidMount() {
+        this.setState({movies: await getMovies()});
+    }
+
     render() {
         return (
-            <div>
-                <p>Movie page</p>
+            <div className="movies full-width blurry-background-white border-radius">
+                <div className="header flex-row">
+                    <h2>ID</h2>
+                    <h2>Description</h2>
+                    <h2>Length</h2>
+                    <h2>Language</h2>
+                    <h2>Name</h2>
+                    <h2>Release date</h2>
+                </div>
+                <div className="rows">
+                    {this.state.movies.length !== 0 ? this.state.movies.map((x, index) => 
+                    (
+                        <div className="row flex-row" key={index}>
+                            <div>{x.id}</div>
+                            <div>{x.description}</div>
+                            <div>{x.length}</div>
+                            <div>{x.language.name}</div>
+                            <div>{x.name}</div>
+                            <div>{x.releaseDate}</div>
+                        </div>
+                    )) : <div className="no-content">No content...</div>}
+                </div>
             </div>
         );
     }
--- src/views/Person.js
+++ src/views/Person.js
@@ -1,10 +1,42 @@
 import React, {Component} from "react";
+import "../assets/css/Person.css";
+import getPersons from "../api/endpoints/getPersons";
 
 export default class Person extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            persons: []
+        };
+    }
+
+    async componentDidMount() {
+        this.setState({persons: await getPersons()});
+    }
+
     render() {
         return (
-            <div>
-                <p>Person page</p>
+            <div className="persons full-width blurry-background-white border-radius">
+                <div className="header flex-row">
+                    <h2>ID</h2>
+                    <h2>Birth date</h2>
+                    <h2>Birthplace</h2>
+                    <h2>First name</h2>
+                    <h2>Last name</h2>
+                </div>
+                <div className="rows">
+                    {this.state.persons.length !== 0 ? this.state.persons.map((x, index) => 
+                    (
+                        <div className="row flex-row" key={index}>
+                            <div>{x.id}</div>
+                            <div>{x.birthDate}</div>
+                            <div>{x.birthPlace}</div>
+                            <div>{x.firstName}</div>
+                            <div>{x.LastName}</div>
+                        </div>
+                    )) : <div className="no-content">No content...</div>}
+                </div>
             </div>
         );
     }
