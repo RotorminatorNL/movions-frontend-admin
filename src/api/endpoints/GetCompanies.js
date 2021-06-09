816a5ddfb7f4aaa00ac69d4dd10a7bbfc0d1f525
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-06-10 00:44:23 +0200

    Test 3

--- src/api/endpoints/GetCompanies.js
+++ src/api/endpoints/GetCompanies.js
@@ -1,6 +1,6 @@
 import apiRequest from "../apiRequest.js";
 
-async function getCompanies() {
+const getCompanies = async () =>{
     const response = await apiRequest("http://localhost:5000/api/company", "GET");
     if(response.ok && response.status !== 204) {
         return response.json();
